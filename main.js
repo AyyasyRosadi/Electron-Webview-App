acabc87ad812da72753af02f8df4e90286217a34
ayyasyrsdy <ayyasyrsd@gmail.com>
2025-02-06 14:01:46 +0700

    first commit

---
+++ main.js
@@ -0,0 +1,38 @@
+import { app, BrowserWindow, screen } from "electron";
+import dotenv from "dotenv";
+
+dotenv.config();
+function createWindow() {
+  try {
+    const displays = screen.getAllDisplays();
+
+    const monitorWidths = [];
+    const monitorHeights = [];
+
+    for (let i = 0; i < displays.length; i++) {
+      monitorWidths.push(displays[i].bounds.width);
+      monitorHeights.push(displays[i].bounds.height);
+    }
+
+    const width = monitorWidths.reduce((acc, curr) => acc + curr, 0);
+    const height = Math.min(...monitorHeights);
+
+    let win = new BrowserWindow({
+      width: width,
+      height: height,
+      x: 0,
+      y: 0,
+      frame: false,
+      // transparent: true,
+    });
+
+    win.loadURL(process.env.LOAD_URL);
+  } catch (err) {
+    console.log("ERR#in:createWindow", err);
+  }
+}
+
+app
+  .whenReady()
+  .then(createWindow)
+  .catch((err) => console.log("ERR#in:whenReady", err));
